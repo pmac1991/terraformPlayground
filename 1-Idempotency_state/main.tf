ca00ad8ac9bfb638f84402fd02a01d84209c6d49
Piotr Maciejewski <piomac11@gmail.com>
2026-03-12 17:46:42 +0100

    4 - Readd comment for server3

--- 1-Idempotency_state/main.tf
+++ 1-Idempotency_state/main.tf
@@ -8,7 +8,7 @@ resource "local_file" "server2" {
   filename = "${path.module}/result/server2.foo"
 }
 
-resource "local_file" "server3" {
-  content  = "im server 3!"
-  filename = "${path.module}/result/server3.foo"
-}
+#resource "local_file" "server3" {
+#  content  = "im server 3!"
+#  filename = "${path.module}/result/server3.foo"
+#}
